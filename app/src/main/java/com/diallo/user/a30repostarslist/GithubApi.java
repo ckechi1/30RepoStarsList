9c3639529f2657734636c5d5cb8a2a9322631eda
ckechi1 <cdiallo23@gmail.com>
2019-04-23 22:18:25 +0000

    Create and Implement Github Api class using Volley

---
+++ app/src/main/java/com/diallo/user/a30repostarslist/GithubApi.java
@@ -0,0 +1,76 @@
+package com.diallo.user.a30repostarslist;
+
+import android.content.Context;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by octavian on 1/29/17.
+ */
+
+public class GithubApi {
+
+    private static GithubApi mInstance;
+    private Context mContext;
+    private RequestQueue mRequestQueue;
+    public static final String URL = "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc";
+
+
+    private GithubApi() {
+    }
+
+    public synchronized static GithubApi getInstance() {
+        if (mInstance == null) {
+            mInstance = new GithubApi();
+        }
+        return mInstance;
+    }
+
+    public void initialize(Context context) {
+        deinitialize();
+        mContext = context.getApplicationContext();
+        mRequestQueue = Volley.newRequestQueue(mContext);
+        mRequestQueue.start();
+    }
+
+    public void deinitialize() {
+        if (mRequestQueue != null) {
+            mRequestQueue.stop();
+            mRequestQueue = null;
+        }
+    }
+
+    private Context getContext() {
+        return mContext;
+    }
+
+    private void sendRequest(Request request) {
+        request.setRetryPolicy(new DefaultRetryPolicy(10000, 1, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        mRequestQueue.add(request);
+    }
+
+    public void get30StarsGithubRepositories(Response.Listener<JSONObject> successListener, Response.ErrorListener errorListener) {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, URL, null, successListener, errorListener) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Accept", "application/vnd.github.mercy-preview+json");
+                return headers;
+            }
+        };
+        sendRequest(request);
+    }
+}
--- app/src/main/java/com/diallo/user/a30repostarslist/MainActivity.java
+++ app/src/main/java/com/diallo/user/a30repostarslist/MainActivity.java
@@ -2,12 +2,56 @@ package com.diallo.user.a30repostarslist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.google.gson.JsonArray;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textview);
+
+        GithubApi.getInstance().initialize(getApplicationContext());
+
+        GithubApi.getInstance().get30StarsGithubRepositories(new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+                    JSONArray jsonArray = response.getJSONArray("items");
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject employee = jsonArray.getJSONObject(i);
+
+                        String name = employee.getString("name");
+                        int id = employee.getInt("id");
+                        String description = employee.getString("description");
+
+                        textView.append(name + ", " + String.valueOf(id) + ", " + description + "\n\n");
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
     }
 }
